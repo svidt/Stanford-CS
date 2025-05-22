3c73f5d55c64b1ee18ad73ed967800f182c9a16d
svidt <79602784+svidt@users.noreply.github.com>
2025-05-22 20:52:44 +0200

    First lecture added

---
+++ Lecture 01/Lecture 01/Lecture_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture_01App.swift
+//  Lecture 01
+//
+//  Created by Kristian Emil on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Lecture_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
