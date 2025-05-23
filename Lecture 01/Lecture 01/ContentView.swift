4fa490b5a49191832729390c44625a9a9ec6d349
svidt <79602784+svidt@users.noreply.github.com>
2025-05-23 16:14:26 +0200

    Initial Commit - Memorize

--- Lecture 01/Lecture 01/ContentView.swift
+++ Lecture 01/Lecture 01/ContentView.swift
@@ -9,57 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis: [String] = ["🐷", "🦑", "🐯", "🐘", "🐬", "🦇", "🐸", "🦜", "🐠", "🦧", "🦕", "🐣"]
-    
-    @State var cardCount: Int = 4
-    
+
     var body: some View {
-        
-        VStack {
             ScrollView {
                 cards
             }
-            Spacer()
-            cardCountAdjusters
-        }
+
         .padding()
     }
     
     var cards: some View {
         LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self) { index in
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundStyle(.orange)
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-        .padding(.vertical)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button {
-            cardCount += offset
-        } label: {
-            Image(systemName: symbol)
-        }
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover: some View {
-        return cardCountAdjuster(by: -1, symbol: "minus.circle.fill")
-    }
-    
-    var cardAdder: some View {
-        return cardCountAdjuster(by: +1, symbol: "plus.circle.fill")
     }
 }
 
---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kristian Emil on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
